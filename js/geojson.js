f6b18f32e3e813d7b4635357adac77ee457ee5bb
nataleedesotell <nataleedesotell@gmail.com>
2016-03-07 21:59:01 -0600

    progress

--- js/geojson.js
+++ js/geojson.js
@@ -1,7 +1,9 @@
 //issues still needing to be addressed:
-//1: forward/reverse buttons won't work in the legend?
-//2. there is no minimum value for 2011?
-
+//1: forward/reverse buttons won't work in the legend
+//2. get 5th operator functioning!!!!
+//3. get video functioning? not a priority though
+//4. how do I bind user to certain area? 
+//5. organize/simplify code
 
 
 //5th interaction operator is at the bottom of this page commented out 
